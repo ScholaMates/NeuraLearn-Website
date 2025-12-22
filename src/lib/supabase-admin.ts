ea2bccb90cdafe05145ea2f9cb519952c69d4885
FabioCanavarro <fabiocanavarrotoh@gmail.com>
2025-12-22 22:43:56 +0700

    Changed config.json to .env

--- src/lib/supabase-admin.ts
+++ src/lib/supabase-admin.ts
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
-import config from '../../config.json'
 
-const supabaseUrl = config.supabaseUrl;
-const supabaseServiceKey = config.supabaseServiceKey;
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseServiceKey = process.env.SUPABASE_SERVICE_ROLE_KEY!;
 
 export const supabaseAdmin = createClient(supabaseUrl, supabaseServiceKey, {
     auth: {
--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -1,14 +1,14 @@
 
 import { createServerClient } from '@supabase/ssr'
 import { cookies } from 'next/headers'
-import config from '../../../config.json'
+
 
 export async function createClient() {
     const cookieStore = await cookies()
 
     return createServerClient(
-        config.supabaseUrl,
-        config.supabaseAnonKey,
+        process.env.NEXT_PUBLIC_SUPABASE_URL!,
+        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
         {
             cookies: {
                 getAll() {
