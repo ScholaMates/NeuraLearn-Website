fabacae77e239c1e52db592a4e42ce2326b5212c
AllBitsGoDown <81661600+AllBitsGoDown@users.noreply.github.com>
2026-05-24 07:13:24 +0700

    update video link

--- src/components/VideoSection.tsx
+++ src/components/VideoSection.tsx
@@ -9,10 +9,10 @@ export default function VideoSection() {
           <div className="aspect-video w-full bg-mocha-crust">
             <iframe
               className="w-full h-full"
-              src="https://www.youtube.com/embed/518V7QLLXm0?si=MsKWYB1qYcFjG9-h"
+              src="https://www.youtube.com/embed/_e5FRYWAW2w?si=fH6JovE30XQm53n-"
               title="NeuraLearn Demo"
               style={{ border: 0 }}
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-medi\u003Cdiv className=\"aspect-video w-full bg-mocha-crust\">a; gyroscope; picture-in-picture; web-share"
               referrerPolicy="strict-origin-when-cross-origin"
               allowFullScreen
             ></iframe>
